4d401373eb651a276dd9fd261c464905200235f9
laililmahfud <laililmahvut@gmail.com>
2022-08-15 11:13:49 +0700

    update api route

--- src/routes/api.php
+++ src/routes/api.php
@@ -12,13 +12,17 @@ Route::prefix(config('starterkit.app_api_prefix'))->group(function () {
         Route::get('/token/renew', 'renewToken')->name('api.renew-token');
     });
 
-    $routeAppsApi = include base_path('routes/starterkit_api.php');
-    foreach ($routeAppsApi as $prefix => $api) {
-        $controller = $api['controller'];
-        $role = $api['role'];
-        $controller = 'App\\Http\\Controllers\Api\\' . $controller;
-        Route::middleware([ApiMiddleware::class])->group(function () use($controller,$prefix) {
-            Starterkit::routeApiController($prefix, $controller);
-        });
+    try {
+        $routeAppsApi = include base_path('routes/starterkit_api.php');
+        foreach ($routeAppsApi as $prefix => $api) {
+            $controller = $api['controller'];
+            $role = $api['role'];
+            $controller = 'App\\Http\\Controllers\Api\\' . $controller;
+            Route::middleware([ApiMiddleware::class])->group(function () use($controller,$prefix) {
+                Starterkit::routeApiController($prefix, $controller);
+            });
+        }
+    } catch (Exception $e) {
+
     }
 });
